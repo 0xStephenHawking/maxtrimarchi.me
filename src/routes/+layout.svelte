b590e1d7717200eeca0b993337307fc34fcab46b
0xStephenHawking <maxipaltri@gmail.com>
2025-09-19 15:26:52 +0200

    pico-css init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import '@styles/base.scss';
+
+	injectSpeedInsights();
+</script>
+
+<svelte:head>
+	<title>MaxTrimarchi - Hi</title>
+</svelte:head>
+
+<main class="container">
+	<slot />
+</main>
